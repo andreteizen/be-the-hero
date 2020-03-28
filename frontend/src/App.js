abbd7f2eea0887ef2c77ecf99bf0557d90c16a16
Andre Teizen <andre.teizen@gmail.com>
2020-03-28 17:48:46 -0300

    Front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import './global.css';
-import Logon from './pages/Logon'; //Ele sempre busca o arquivo index dentro da pasta, caso nao seja passado um arquivo para ele
+
+import Routes from './routes';
 
 /**
  * Componente no react nada mais é do que uma FUNÇÃO que retorna um HTML
@@ -11,7 +12,7 @@ function App() {
   // useState retorna um array [valor, função de atualização]
 
   return (
-    <Logon />
+    <Routes />
   );
 }
 
